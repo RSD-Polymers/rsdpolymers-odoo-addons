3068ab515a17f9253638ca7ad10428710842e51e
Ameya Vaidya <ameyav123@gmail.com>
2025-06-17 19:04:58 +0530

    Project Task - Remove start button from task form view

--- project_customization/__manifest__.py
+++ project_customization/__manifest__.py
@@ -3,13 +3,13 @@
     'version': '1.0',
     'category': 'Project',
     'author': 'RSD Polymers',
-    'depends': ['project', 'web', 'hr_timesheet'],
+    'depends': ['project', 'web', 'project_enterprise', 'hr_timesheet', 'timesheet_grid'],
     'data': [
         'security/ir.model.access.csv',
         'views/project_task_views.xml',
         'views/res_groups_view.xml',
     ],
-    'assets': {  # Define your custom assets here
+    'assets': {
         'web.assets_backend': [
             'project_customization/static/src/js/project_task_state_selection_extend.js',
         ],
